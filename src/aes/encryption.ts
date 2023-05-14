c3a2244480a80726ccaeef13a28e010ca109779f
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-05-14 12:28:19 -0400

    feat: update encryption algo

--- src/aes/encryption.ts
+++ src/aes/encryption.ts
@@ -4,12 +4,13 @@ import { createCipheriv, createDecipheriv, randomBytes } from 'node:crypto';
 const data = 'foo';
 const key = randomBytes(32);
 const iv = randomBytes(16);
-const cipher = createCipheriv('aes-256-gcm', key, iv);
-const decipher = createDecipheriv('aes-256-gcm', key, iv);
-const cipherText = cipher.update(data, 'utf-8', 'base64');
-const plainText = decipher
-  .update(Buffer.from(cipherText, 'base64'))
-  .toString('utf-8');
+const cipher = createCipheriv('aes-256-cbc', key, iv);
+const decipher = createDecipheriv('aes-256-cbc', key, iv);
+
+const cipherText =
+  cipher.update(data, 'utf-8', 'base64') + cipher.final('base64');
+const plainText =
+  decipher.update(cipherText, 'base64', 'utf-8') + decipher.final('utf-8');
 
 console.log(key.toString('base64'));
 console.log(iv.toString('base64'));
