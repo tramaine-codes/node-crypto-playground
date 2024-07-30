1ff3e9cde4e07e0776d6fa6b4b761c4fa18cf073
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-07-29 20:21:08 -0400

    refactor: reorganize

--- src/crypto/oauth/issuer.ts
+++
@@ -1,7 +0,0 @@
-import { Issuer } from 'openid-client';
-import { env } from '../../infrastructure/env.js';
-
-const issuer = await Issuer.discover(env.AUTH0_ISSUER);
-
-// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-console.log(issuer.metadata);
